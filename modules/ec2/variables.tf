d7e3121af53d058d09bb1cf2771c7719f9160b59
Vlad <master.neo16@gmail.com>
2025-02-02 17:34:45 +0200

    add cicd

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "key_name" {
+  description = "The name of the key pair to use"
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet"
+}
+
+variable "security_group_id" {
+  description = "The ID of the security group"
+}
